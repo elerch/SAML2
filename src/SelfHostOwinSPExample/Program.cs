299b8a9a0d8cb6c01d5e176021b5dfe69c429a76
Emil Lerch <emil@lerch.org>
2015-01-12 14:45:14 -0800

    added basic Owin sample

---
+++ src/SelfHostOwinSPExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace SelfHostOwinSPExample
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Title = "SelfHost Sample";
+
+            const string url = "https://localhost:44333/core";
+            using (WebApp.Start<Startup>(url)) {
+                Console.WriteLine("\n\nServer listening at {0}. Press enter to stop", url);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ src/SelfHostOwinSPExample/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Owin;
+using SAML2.Config;
+
+namespace SelfHostOwinSPExample
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            appBuilder.UseSamlAuthentication(new Owin.Security.Saml.SamlAuthenticationOptions
+            {
+                AuthenticationMode = Microsoft.Owin.Security.AuthenticationMode.Active,
+                Configuration = GetSamlConfiguration()
+            });
+            appBuilder.Run(c => {
+                if (c.Authentication.User != null &&
+                    c.Authentication.User.Identity != null &&
+                    c.Authentication.User.Identity.IsAuthenticated) {
+                    return c.Response.WriteAsync("hello world - authenticated");
+                } else {
+                    // trigger authentication
+                    c.Authentication.Challenge(c.Authentication.GetAuthenticationTypes().Select(d => d.AuthenticationType).ToArray());
+                    return System.Threading.Tasks.Task.Delay(0);
+                }
+            });
+        }
+
+        private Saml2Configuration GetSamlConfiguration()
+        {
+            var myconfig = new Saml2Configuration();
+            SAML2.Logging.LoggerProvider.Configuration = myconfig;
+            return myconfig;
+        }
+    }
+}
