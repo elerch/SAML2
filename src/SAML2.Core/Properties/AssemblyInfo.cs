632c8c688244dacf718f705c3bef73bea098a9fd
Emil Lerch <emil@lerch.org>
2015-01-13 07:47:35 -0800

    removing unnecessary dependencies

--- src/SAML2.Core/Properties/AssemblyInfo.cs
+++ src/SAML2.Core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SAML2")]
-[assembly: AssemblyDescription("A .NET implementation of the SAML 2.0 specification for SP integrations.")]
+[assembly: AssemblyDescription("Based on http://www.nuget.org/packages/SAML2/, this removes dependencies on System.Web. Packages SAML2.AspNet and Owin.Security.Saml will implement SAML in those environments")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Emil Lerch")]
 [assembly: AssemblyProduct("SAML2.Core")]
