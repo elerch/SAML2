28ab440f2a084f15abffb0131a7a042ce315db69
Emil Lerch <emil@lerch.org>
2015-02-06 09:41:46 -0800

    start support for a test configuration in sample

--- src/SelfHostOwinSPExample/Program.cs
+++ src/SelfHostOwinSPExample/Program.cs
@@ -8,8 +8,10 @@ namespace SelfHostOwinSPExample
         private static void Main(string[] args)
         {
             Console.Title = "SelfHost Sample";
-
-            const string url = "https://localhost:44333/core";
+            var url = "https://localhost:44333/core";
+#if TEST
+            url = "http://localhost:7777/identity";
+#endif
             using (WebApp.Start<Startup>(url)) {
                 Console.WriteLine("\n\nServer listening at {0}. Press enter to stop", url);
                 Console.ReadLine();
