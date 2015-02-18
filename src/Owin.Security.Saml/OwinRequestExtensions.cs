82bb2f49f304162251a9fb6e9c3817edfb91ac73
Emil Lerch <emil@lerch.org>
2015-02-17 16:38:48 -0800

    fix for hang on postback seen during integration
    See another project with this issue:
    
    https://github.com/damianh/LibOwin/issues/12

--- src/Owin.Security.Saml/OwinRequestExtensions.cs
+++ src/Owin.Security.Saml/OwinRequestExtensions.cs
@@ -60,7 +60,7 @@ namespace Owin.Security.Saml
         {
             var dictionary = new Dictionary<string, string>(StringComparer.CurrentCultureIgnoreCase);
 
-            var formCollectionTask = request.ReadFormAsync();
+            var formCollectionTask = ReadFormAsync(request);
 
             formCollectionTask.Wait();
 
@@ -73,6 +73,79 @@ namespace Owin.Security.Saml
             return dictionary;
         }
 
+        public static Task<IFormCollection> ReadFormAsync(this IOwinRequest value)
+        {
+            var form = value.Get<IFormCollection>("Microsoft.Owin.Form#collection");
+            if (form == null) {
+                value.Body.Seek(0, System.IO.SeekOrigin.Begin); // needed so ReadToEndAsync() doesn't hang
+                string text;
+                // Don't close, it prevents re-winding.
+                using (var reader = new System.IO.StreamReader(value.Body, Encoding.UTF8, detectEncodingFromByteOrderMarks: true, bufferSize: 4 * 1024, leaveOpen: true)) {
+                    // *not* async - we're seeing hangs.  See another with this issue: https://github.com/damianh/LibOwin/issues/12
+                    text = reader.ReadToEnd();
+                }
+                value.Body.Seek(0, System.IO.SeekOrigin.Begin); // subsequent calls will return nothing if this isn't re-wound
+                form = GetForm(text);
+                value.Set("Microsoft.Owin.Form#collection", form);
+            }
+            return Task.FromResult(form);
+        }
+
+        #region Lifted from Katana project internal class. Microsoft.Owin.Infrastructure.OwinHelpers
+        internal static IFormCollection GetForm(string text)
+        {
+            IDictionary<string, string[]> form = new Dictionary<string, string[]>(StringComparer.OrdinalIgnoreCase);
+            var accumulator = new Dictionary<string, List<string>>(StringComparer.OrdinalIgnoreCase);
+            ParseDelimited(text, new[] { '&' }, AppendItemCallback, accumulator);
+            foreach (var kv in accumulator) {
+                form.Add(kv.Key, kv.Value.ToArray());
+            }
+            return new FormCollection(form);
+        }
+
+        private static readonly Action<string, string, object> AppendItemCallback = (name, value, state) => {
+            var dictionary = (IDictionary<string, List<String>>)state;
+
+            List<string> existing;
+            if (!dictionary.TryGetValue(name, out existing)) {
+                dictionary.Add(name, new List<string>(1) { value });
+            } else {
+                existing.Add(value);
+            }
+        };
+
+        internal static void ParseDelimited(string text, char[] delimiters, Action<string, string, object> callback, object state)
+        {
+            int textLength = text.Length;
+            int equalIndex = text.IndexOf('=');
+            if (equalIndex == -1) {
+                equalIndex = textLength;
+            }
+            int scanIndex = 0;
+            while (scanIndex < textLength) {
+                int delimiterIndex = text.IndexOfAny(delimiters, scanIndex);
+                if (delimiterIndex == -1) {
+                    delimiterIndex = textLength;
+                }
+                if (equalIndex < delimiterIndex) {
+                    while (scanIndex != equalIndex && char.IsWhiteSpace(text[scanIndex])) {
+                        ++scanIndex;
+                    }
+                    string name = text.Substring(scanIndex, equalIndex - scanIndex);
+                    string value = text.Substring(equalIndex + 1, delimiterIndex - equalIndex - 1);
+                    callback(
+                        Uri.UnescapeDataString(name.Replace('+', ' ')),
+                        Uri.UnescapeDataString(value.Replace('+', ' ')),
+                        state);
+                    equalIndex = text.IndexOf('=', delimiterIndex);
+                    if (equalIndex == -1) {
+                        equalIndex = textLength;
+                    }
+                }
+                scanIndex = delimiterIndex + 1;
+            }
+        }
+        #endregion
         /// <summary>
         /// Gets the header request parameters.
         /// </summary>
