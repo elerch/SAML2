45a5557c8e8b7ab82cc372b1fcd1c4f527b81068
Emil Lerch <emil@lerch.org>
2015-02-20 13:40:00 -0800

    add subject information as additional claims

--- src/Owin.Security.Saml/Saml20AssertionExtensions.cs
+++ src/Owin.Security.Saml/Saml20AssertionExtensions.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Linq;
 using System.Security.Claims;
+using SAML2.Schema.Core;
+using System.Collections.Generic;
 
 namespace Owin.Security.Saml
 {
@@ -10,7 +12,16 @@ namespace Owin.Security.Saml
         public static ClaimsIdentity ToClaimsIdentity(this Saml20Assertion value, string authenticationType, string nameType = null, string roleType = null)
         {
             if (value == null) throw new ArgumentNullException("value"); 
-            return new ClaimsIdentity(value.Attributes.Select(a => a.ToClaim(value.Issuer)), authenticationType, nameType, roleType);
+            return new ClaimsIdentity(value.Attributes.Select(a => a.ToClaim(value.Issuer)).Concat(ClaimsFromSubject(value.Subject, value.Issuer)), authenticationType, nameType, roleType);
+        }
+
+        private static IEnumerable<Claim> ClaimsFromSubject(NameId subject, string issuer)
+        {
+            if (subject == null) yield break;
+            if (subject.Value != null) {
+                yield return new Claim("sub", subject.Value, ClaimValueTypes.String, issuer); // openid connect
+                yield return new Claim(ClaimTypes.NameIdentifier, subject.Value, ClaimValueTypes.String, issuer); // saml
+            }
         }
     }
 }
