17470ce8fe9e68a4c50b9bcd0a1a2610faf808f4
Tomasz Maruszak <maruszaktomasz@gmail.com>
2016-03-10 15:03:39 +0100

    Fixing the RedirectUrl which was not reconstructed properly when it contained and equal (=) sign.

---
+++ src/Owin.Security.Saml.Test/DictionaryExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Owin.Security.Saml.Test
+{
+    /// <summary>
+    /// Unit test for <see cref="IDictionaryExtensions"/>
+    /// </summary>
+    [TestClass]
+    public class DictionaryExtensionsTests
+    {
+        [TestMethod]
+        public void EscapesValuesProperly()
+        {
+            const string key = ".return";
+            const string value = "http://localhost:9000/Auth/SignIn?returnUrl=http%3A%2F%2Flocalhost%3A9000%2F%23%2Fhome";
+            
+            
+            // arrange
+            var attributes = new Dictionary<string, string>()
+            {
+                {key, value}
+            };
+
+            // act
+            var attributesAsString = attributes.ToDelimitedString();
+            var newAttributesFromString = attributesAsString.FromDelimitedString().ToDictionary(x => x.Key, x => x.Value);
+
+            // assert
+            Assert.AreEqual(attributes[key], newAttributesFromString[key]);
+        }
+    }
+}
